ba6b5bb39af01151afb3642c4a378a030944c008
YutoKaneko <ctne.aoh.hmcy.127@gmail.com>
2016-04-17 07:18:23 +0000

    バグ修正　gem追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'rss#news'
-
+  devise_for :users, :controllers => {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations',
+  omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  devise_for :authors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
